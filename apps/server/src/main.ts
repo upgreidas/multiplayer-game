c03c33926101414f6c2e3879401e7ba6d41867f6
upgreidas <gungaudas@gmail.com>
2020-08-05 18:22:05 +0300

    Setup websocket server

--- apps/server/src/main.ts
+++ apps/server/src/main.ts
@@ -1,1 +1,14 @@
-console.log('Hello World!');
+import { Server } from 'ws';
+import { Duplex } from 'stream';
+
+const port = parseInt(process.env.SERVER_PORT) || 8000;
+
+const wss = new Server({ port });
+
+wss.on('connection', (ws: Duplex) => {
+  ws.on('message', (message: MessageEvent) => {
+    console.log('received: %s', message);
+  });
+});
+
+console.log(`Server is listening on ${port} port.`);
