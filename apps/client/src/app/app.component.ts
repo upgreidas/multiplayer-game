875c6e659b545d25cf2af112a3451ce57319497c
upgreidas <gungaudas@gmail.com>
2020-08-05 15:48:22 +0300

    Setup project

---
+++ apps/client/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'multiplayer-game-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'client';
+}
