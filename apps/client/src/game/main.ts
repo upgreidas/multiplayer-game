db235d65854ffd8b5bc598585d5ad44f0c43995d
upgreidas <gungaudas@gmail.com>
2020-08-12 23:25:47 +0300

    Clean up code

--- apps/client/src/game/main.ts
+++ apps/client/src/game/main.ts
@@ -6,27 +6,13 @@ import EntityManager from './entity-manager';
 import ConnectionManager from './connection-manager';
 import Physics from './physics';
 import Controls from './controls';
-import { Entity } from './entity';
-import { EntityData } from './interfaces/entity-data';
 
 const entities = [];
 
 let playerId: string;
-let cameraAttached = false;
-
-const addEntity = (data: EntityData) => {
-  const entity = new Entity(data);
-
-  entity.build();
-
-  entities.push(entity);
-
-  return entity;
-};
 
 const init = async (container: HTMLElement) => {
   SceneManager.init(container);
-  Physics.init();
 
   const scene = SceneManager.getScene();
   const camera = SceneManager.getCamera();
@@ -39,34 +25,12 @@ const init = async (container: HTMLElement) => {
   camera.position.set(0, 22, -19);
   camera.rotation.set(Math.PI / 4, 0, 0);
 
+  Physics.init();
+  
   SceneManager.render();
 
   Controls.init();
-  Controls.onMove(async ({x, y}) => {
-    let direction = 0;
-
-    if(x === 0 && y === 1) {
-      direction = 1;
-    } else if(x === 1 && y === 1) {
-      direction = 2;
-    } else if(x === 1 && y === 0) {
-      direction = 3;
-    } else if(x === 1 && y === -1) {
-      direction = 4;
-    } else if(x === 0 && y === -1) {
-      direction = 5;
-    } else if(x === -1 && y === -1) {
-      direction = 6;
-    } else if(x === -1 && y === 0) {
-      direction = 7;
-    } else if(x === -1 && y === 1) {
-      direction = 8;
-    }
-    
-    ConnectionManager.send({
-      move: { direction },
-    })
-  });
+  Controls.onMove(onMove);
 
   scene.registerBeforeRender(() => {
     const dt = SceneManager.getDeltaTime();
@@ -75,39 +39,66 @@ const init = async (container: HTMLElement) => {
   });
 
   ConnectionManager.connect('ws://localhost:8000');
+  ConnectionManager.onConnect(onConnect);
+  ConnectionManager.onMessage(onMessage);
+};
 
-  ConnectionManager.onConnect(() => {
-    ConnectionManager.send({
-      login: {
-        name: 'test',
+const onMove = ({x, y}) => {
+  let direction = 0;
+
+  if(x === 0 && y === 1) {
+    direction = 1;
+  } else if(x === 1 && y === 1) {
+    direction = 2;
+  } else if(x === 1 && y === 0) {
+    direction = 3;
+  } else if(x === 1 && y === -1) {
+    direction = 4;
+  } else if(x === 0 && y === -1) {
+    direction = 5;
+  } else if(x === -1 && y === -1) {
+    direction = 6;
+  } else if(x === -1 && y === 0) {
+    direction = 7;
+  } else if(x === -1 && y === 1) {
+    direction = 8;
+  }
+  
+  ConnectionManager.send({
+    move: { direction },
+  });
+};
+
+const onConnect = () => {
+  ConnectionManager.send({
+    login: {
+      name: 'test',
+    }
+  });
+};
+
+const onMessage = (data: any) => {
+  if(data.players) {
+    data.players.forEach(state => {
+      let player = EntityManager.getEntity(state.id);
+
+      if(player) {
+        player.applyState(state);
+      } else {
+        state.model = 'character';
+
+        player = EntityManager.addEntity(state);
+
+        if(state.id === playerId) {
+          player.attachCamera(camera);
+        }
       }
     });
-  });
+  }
 
-  ConnectionManager.onMessage((data: any) => {
-    if(data.players) {
-      data.players.forEach(state => {
-        let player = EntityManager.getEntity(state.id);
-  
-        if(player) {
-          player.applyState(state);
-        } else {
-          state.model = 'character';
-  
-          player = EntityManager.addEntity(state);
-
-          if(state.id === playerId) {
-            player.attachCamera(camera);
-          }
-        }
-      });
-    }
-
-    if(data.id) {
-      playerId = data.id;
-    }
-  });
-
+  if(data.id) {
+    playerId = data.id;
+  }
 };
 
 export default {
